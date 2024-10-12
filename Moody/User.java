c09ecd5946d6d8df71429e038bb07a9bd2d109f5
Oquba Khan <oqubak@umich.edu>
2024-10-12 17:44:20 -0400

    Created User class

---
+++ Moody/User.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private String bio;
+    private String gender;
+    private int age;
+    private String mood;
+    List<String> medicalHistory = new ArrayList();
+
+    public User(String u, String b, String g, int a, String m) {
+        this.username = u;
+        this.bio = b;
+        this.gender = g;
+        this.age = a;
+        this.mood = m;
+    }
+
+    public void setUsername(String u) {
+        // Maybe make function to check to see if the username is taken.
+        this.username = u;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setBio(String b) {
+        this.bio = b;
+    }
+    public String getBio() {
+        return this.bio;
+    }
+
+    public void setGender(String g) {
+        this.gender = g;
+    }
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setAge(int a) {
+        this.age = a;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setMood(String m) {
+        this.mood = m;
+    }
+    public String getMood() {
+        return this.mood;
+    }
+}

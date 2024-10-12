5e57d3ea268cf3e2fb6e3287f299dd5c6945349f
Oquba Khan <oqubak@umich.edu>
2024-10-12 19:50:31 -0400

    Created community class

---
+++ Moody/Models/Community.java
@@ -0,0 +1,12 @@
+package Moody.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Community {
+    List<Thread> communityPosts = new ArrayList<>();
+
+    public void addNewpost(Thread post) {
+        communityPosts.add(post);
+    } 
+}
